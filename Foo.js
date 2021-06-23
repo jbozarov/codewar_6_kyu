243aa2816ee80fb353960a67314d7b4206aa6419
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-06-22 21:41:37 -0400

    Update and rename foo.js to Foo.js

--- Foo.js
+++ Foo.js
@@ -1,4 +1,4 @@
-The range of vision of a digit is its own value. 1 can see one digit to the left and one digit to the right,2 
+/* The range of vision of a digit is its own value. 1 can see one digit to the left and one digit to the right,2 
 can see two digits, and so on.
 
 Thus, the loneliness of a digit N is the sum of the digits which it can see.
@@ -14,6 +14,8 @@ digit	can see on the left	can see on the right	loneliness
 3	34	15	3 + 4 + 1 + 5 = 13
 1	3	5	3 + 5 = 8
 5	3431	-	3 + 4 + 3 + 1 = 11
+
+*/ 
 
 
 function foo (n) {
