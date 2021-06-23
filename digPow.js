02e911b16692d6054189b5b735210ad5c6bf03bc
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-06-22 21:42:00 -0400

    Update digPow.js

--- digPow.js
+++ digPow.js
@@ -1,4 +1,4 @@
-Some numbers have funny properties. For example:
+/* Some numbers have funny properties. For example:
 
 89 --> 8¹ + 9² = 89 * 1
 
@@ -17,8 +17,7 @@ Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...)
 If it is the case we will return k, if not return -1.
 
 Note: n and p will always be given as strictly positive integers.
-
-
+*/ 
 
 
 function digPow(n, p){
