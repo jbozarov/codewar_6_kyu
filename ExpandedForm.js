3ab0af50d37e5668ac8e89e65b858f1d3d1d9caf
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-06-22 21:43:07 -0400

    Update and rename expandedForm.js to ExpandedForm.js

--- ExpandedForm.js
+++ ExpandedForm.js
@@ -1,9 +1,10 @@
-Write Number in Expanded Form
+/* Write Number in Expanded Form
 You will be given a number and you will need to return it as a string in Expanded Form. For example:
 
 expandedForm(12); // Should return '10 + 2'
 expandedForm(42); // Should return '40 + 2'
 expandedForm(70304); // Should return '70000 + 300 + 4'
+*/ 
 
 
 
