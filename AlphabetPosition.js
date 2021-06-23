6977ed1edc18b21fbdabaa027d3b36cbc355e344
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-06-22 21:40:45 -0400

    Update and rename alphabetPosition.js to AlphabetPosition.js

--- AlphabetPosition.js
+++ AlphabetPosition.js
@@ -1,4 +1,4 @@
-In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+/* In this kata you are required to, given a string, replace every letter with its position in the alphabet.
 
 If anything in the text isn't a letter, ignore it and don't return it.
 
@@ -6,7 +6,7 @@ If anything in the text isn't a letter, ignore it and don't return it.
 
 Example
 alphabetPosition("The sunset sets at twelve o' clock.")
-Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string) */ 
 
 
 
