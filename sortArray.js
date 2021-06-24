b2ea40b76fbd32e53bfd19d6714c3fe194df36e9
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-06-23 22:38:53 -0400

    Update sortArray.js

--- sortArray.js
+++ sortArray.js
@@ -1,4 +1,4 @@
-You have an array of numbers.
+/* You have an array of numbers.
 Your task is to sort ascending odd numbers but even numbers must be on their places.
 
 Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
@@ -6,6 +6,7 @@ Zero isn't an odd number and you don't need to move it. If you have an empty arr
 Example
 
 sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+*/
 
 function sortArray(array) {
   let odds = array.filter(v => v>0 && v%2 !== 0).sort((a, b) => a-b); 
