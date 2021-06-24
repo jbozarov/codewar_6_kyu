6ac87c5b2fcd168bb86bf3d10690e851b9128581
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-06-23 22:40:45 -0400

    Update and rename toCamelCase.js to ToCamelCase.js

--- ToCamelCase.js
+++ ToCamelCase.js
@@ -1,4 +1,4 @@
-Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
+/* Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
 The first word within the output should be capitalized only if the original word was capitalized (known as 
 Upper Camel Case, also often referred to as Pascal case).
 
@@ -6,7 +6,7 @@ Examples
 toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
 
 toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
-
+*/
 
 
 
