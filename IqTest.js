e3ee1439ab155a4da47165f28743ea3f3ba49be8
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-06-23 22:33:28 -0400

    Update and rename iqTest.js to IqTest.js

--- IqTest.js
+++ IqTest.js
@@ -1,4 +1,4 @@
-Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers 
+/* Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers 
 differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to 
 check his answers, he needs a program that among the given numbers finds one that is different in evenness, and 
 return a position of this number.
@@ -11,7 +11,7 @@ from 1 (not 0)
 iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
 
 iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd
-
+*/ 
 
 
 function iqTest(numbers){
