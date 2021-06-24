336a707dd9dc8bed513c0369289527309f564acd
Jahon Bozarov <53405754+jbozarov@users.noreply.github.com>
2021-06-23 22:38:23 -0400

    Update and rename narcissistic.js to Narcissistic.js

--- Narcissistic.js
+++ Narcissistic.js
@@ -1,4 +1,4 @@
-A Narcissistic Number is a number which is the sum of its own digits, each raised to the power of the number of 
+/* A Narcissistic Number is a number which is the sum of its own digits, each raised to the power of the number of 
 digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
 
 For example, take 153 (3 digits):
@@ -8,7 +8,7 @@ and 1634 (4 digits):
 
     1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634
 
-
+*/
 
 
 function narcissistic(n) {
