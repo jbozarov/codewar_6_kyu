58c8da1b8bc69ee40326918b3d2c96efa2b04c61
jbozarov <tech@jbozarov.com>
2021-09-06 20:44:05 -0400

    Committing

---
+++ twiceAsOld.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+dad_years_old=$1
+son_years_old=$2
+
+count=0
+
+foo() {
+    echo "Ehdkskfs" + $1 
+}
+# if [ "$dad_years_old" > "$son_years_old"*2 ]
+# then
+#     while [ "$dad_years_old" > "$son_years_old"*2 ]
+#     do 
+#         count+=1
+#         dad_years_old+=1
+#         son_years_old+=1
+#         echo count; 
+#     done
+# fi
+
+foo " skdfsdk "
+
+exit
